ce5126a50ad4de2fc8ccef7ca49305d14b9bb12b
TracyNK <95154345+TracyKien@users.noreply.github.com>
2022-05-15 21:15:45 -0500

    Delete queries.sql

--- queries.sql
+++
@@ -1,9 +0,0 @@
--- Create tables for TNK-WineDB
-
-CREATE TABLE winequality_goodvsbad (
-    quality VARCHAR(2) NOT NULL,
-	type VARCHAR(6) NOT NULL,
-	class VARCHAR(5) NOT NULL
-);
-
-SELECT * FROM winequality_goodvsbad;
